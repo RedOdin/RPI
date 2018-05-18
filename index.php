7b23fe2d41a472c712951c8aae462795ad4be715
RedOdin <anton_post@mail.ru>
2018-05-18 16:16:39 +0300

    Add index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>resize</title>
+        <style>
+            textarea { resize: none; }
+        </style>
+    </head>
+    <body>
+        <h1>Working with data bases</h1>
+        <h2>Inquiry:</h2>
+        <form method="POST">
+            <p><textarea autofocus required wrap="hard" cols="100" rows="15" type="text" name="Inquiry" /></textarea></p>
+            <p><font size = "5"><b>Table:</b></font></p>
+            <input type="text" size = "50" name="Table" /></p>
+            <input type="submit" value="Send" name="button">
+        </form>
+    </body>
+</html>
